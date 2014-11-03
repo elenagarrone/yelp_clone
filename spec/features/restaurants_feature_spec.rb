e81bca0948237d1c88d0ca4b8e1227e451127641
elena <elena.15@hotmail.it>
2014-11-03 16:17:39 +0000

    can view the page of the restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -27,17 +27,29 @@ describe 'restaurants' do
 
 	end
 
-end
 
-describe 'creating restaurants' do 
+		it 'prompts user to fill out a form, then displays the new restaurant' do
+			visit '/restaurants'
+			click_link 'Add a restaurant'
+			fill_in 'Name', with: 'KFC'
+			click_button 'Create Restaurant'
+			expect(page).to have_content 'KFC'
+			expect(current_path).to eq '/restaurants'
+		end
 
-	it 'prompts user to fill out a form, then displays the new restaurant' do
-		visit '/restaurants'
-		click_link 'Add a restaurant'
-		fill_in 'Name', with: 'KFC'
-		click_button 'Create Restaurant'
-		expect(page).to have_content 'KFC'
-		expect(current_path).to eq '/restaurants'
+	context 'viewing restaurants' do
+
+		before do
+			@kfc = Restaurant.create(name:'KFC')
+		end
+
+		it 'lets a user view a restaurant' do
+			visit '/restaurants'
+			click_link 'KFC'
+			expect(page).to have_content 'KFC'
+			expect(current_path).to eq "/restaurants/#{@kfc.id}"
+		end
 	end
 end
+
 
