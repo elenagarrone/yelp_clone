463884ede9e59b50981fb49546fd1a057845087b
elena <elena.15@hotmail.it>
2014-11-07 15:07:12 +0000

    avarage review with the stars on homepage

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,6 @@
 module ReviewsHelper
 
-  def start_rating(rating)
+  def star_rating(rating)
     return rating unless rating.respond_to?(:round)
     remainder = (5 - rating)
     "★" * rating.round +  "☆" * remainder
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -5,15 +5,15 @@ describe ReviewsHelper do
   context "#star_rating" do
     
     it 'does nothing for not a number' do
-      expect(helper.start_rating('N/A')).to eq 'N/A'
+      expect(helper.star_rating('N/A')).to eq 'N/A'
     end
 
     it 'returns three black stars and two white stars for three' do
-      expect(helper.start_rating(3)).to eq '★★★☆☆'
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
     end
 
     it 'returns four black stars and one white star for 3.5' do
-      expect(helper.start_rating(3.5)).to eq '★★★★☆'
+      expect(helper.star_rating(3.5)).to eq '★★★★☆'
     end
 
   end
